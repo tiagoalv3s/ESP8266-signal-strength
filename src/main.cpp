736a9c4772a062cca33d71d76fca2b89e3d891b9
tiagoalv3s <102432726+tiagoalv3s@users.noreply.github.com>
2023-01-04 09:29:34 -0300

    Add WiFi libraries and setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <DNSServer.h>
+
+const char* ssid = "ssid";
+const char* password = "password";
 
 void setup() {
-
+    Serial.begin(115200);
+    delay(10);
+    Serial.print("Connecting to WiFi");
+    WiFi.begin( ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(100);
+        Serial.print(".");
+    }
+    Serial.println(" Connected!");
 }
 
 void loop() {
